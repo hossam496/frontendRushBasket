f55682d610a98d2346b9d6e2e44eff292375bdde
Hossam Albasuony <albasuonyh@gmail.com>
2026-03-22 12:50:48 +0200

    update project

--- vite.config.js
+++ vite.config.js
@@ -19,12 +19,30 @@ export default defineConfig({
           charts: ['chart.js', 'react-chartjs-2', 'recharts'],
           payment: ['@stripe/react-stripe-js', '@stripe/stripe-js'],
           utils: ['axios', 'socket.io-client']
+        },
+        // Optimize asset file names for better caching
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/\.(mp4|webm|ogg|mp3|wav|flac|aac)$/.test(assetInfo.name)) {
+            return `media/[name]-[hash][extname]`;
+          }
+          if (/\.(png|jpe?g|gif|svg|webp|avif)$/.test(assetInfo.name)) {
+            return `images/[name]-[hash][extname]`;
+          }
+          if (/\.css$/.test(assetInfo.name)) {
+            return `css/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
         }
       }
     },
     minify: 'terser',
     sourcemap: false,
-    chunkSizeWarningLimit: 1000
+    chunkSizeWarningLimit: 1000,
+    // Optimize assets
+    assetsInlineLimit: 4096, // Inline assets smaller than 4KB
+    cssCodeSplit: true
   },
   server: {
     host: true,
