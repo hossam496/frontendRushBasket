866637470b3d5933847519555ed6125576b17564
Hossam Albasuony <albasuonyh@gmail.com>
2026-03-31 18:26:43 +0200

    update project

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom' // أضف هذا السطر
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { NotificationProvider } from './context/NotificationContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> 
-      <AuthProvider> 
-        <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <NotificationProvider>
+          <App />
+        </NotificationProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
