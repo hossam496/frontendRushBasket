3759be60c43a1c7c3ee441c74fe8a660fa712193
Hossam Albasuony <albasuonyh@gmail.com>
2026-03-31 12:51:08 +0200

    update project

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider> 
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
