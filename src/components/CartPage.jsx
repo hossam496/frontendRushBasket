4b96467466d9b2e20a2f21dfa8ed806e415a8250
Hossam Albasuony <albasuonyh@gmail.com>
2026-04-01 10:56:59 +0200

    update project

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -36,7 +36,7 @@ const CartPage = () => {
               Looks like you haven’t added any organic goodies to your cart yet
             </p>
             <Link to='/items' className={cartStyles.emptyCartButton}>
-            Browes Products
+              Browes Products
             </Link>
           </div>
         </div>
@@ -44,100 +44,100 @@ const CartPage = () => {
     );
   }
   return (
-      <div className={cartStyles.pageContainer}>
-        <div className={cartStyles.maxContainerLarge}>
-          <div className={cartStyles.headerContainer}>
-            <h1 className={cartStyles.headerTitle}>
-              Your Shopping Cart
-            </h1>
-            <button onClick={clearCart} className={cartStyles.clearCartButton}>
-              <FiTrash2 className="mr-1" />
-              Clear Cart
-            </button>
+    <div className={cartStyles.pageContainer}>
+      <div className={cartStyles.maxContainerLarge}>
+        <div className={cartStyles.headerContainer}>
+          <h1 className={cartStyles.headerTitle}>
+            Your Shopping Cart
+          </h1>
+          <button onClick={clearCart} className={cartStyles.clearCartButton}>
+            <FiTrash2 className="mr-1" />
+            Clear Cart
+          </button>
+        </div>
+
+        <div className={cartStyles.cartGrid}>
+          <div className={cartStyles.cartItemsSection}>
+            <div className={cartStyles.cartItemsGrid}>
+              {cart.map((item) => (
+                <div key={item.id} className={cartStyles.cartItemCard}>
+                  <div className={cartStyles.cartItemImageContainer}>
+                    <img src={item.image} alt={item.name} className={cartStyles.cartItemImage} />
+                  </div>
+                  <h3 className={cartStyles.cartItemName}>{item.name}</h3>
+                  <p className={cartStyles.cartItemPrice}>
+                    ${(item.price ?? 0).toFixed(2)}
+                  </p>
+
+                  {/* add controls */}
+                  <div className={cartStyles.cartItemQuantityContainer}>
+                    <button onClick={() => handleQuantityChange(item.id, -1)}
+                      className={cartStyles.cartItemQuantityButton}>
+                      <FiMinus />
+                    </button>
+                    <span className={cartStyles.cartItemQuantity}></span>
+                    <button onClick={() => handleQuantityChange(item.id, 1)}
+                      className={cartStyles.cartItemQuantity}>
+                      <FiPlus />
+                    </button>
+                  </div>
+
+                  <button onClick={() => removeFromCart(item.id)}
+                    className={cartStyles.cartItemRemoveButton}>
+                    <FiTrash2 className="mr-1" />
+                    Remove
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
 
-          <div className={cartStyles.cartGrid}>
-            <div className={cartStyles.cartItemsSection}>
-              <div className={cartStyles.cartItemsGrid}>
-                {cart.map((item) => (
-                  <div key={item.id} className={cartStyles.cartItemCard}>
-                    <div className={cartStyles.cartItemImageContainer}>
-                      <img src={item.image} alt={item.name} className={cartStyles.cartItemImage} />
-                    </div>
-                    <h3 className={cartStyles.cartItemName}>{item.name}</h3>
-                    <p className={cartStyles.cartItemPrice}>
-                      ${(item.price ?? 0).toFixed(2)}
-                    </p>
+          {/* order summery */}
+          <div className="lg:col-span-1">
+            <div className={cartStyles.orderSummaryCard}>
+              <h2 className={cartStyles.orderSummaryTitle}>Order Summery</h2>
 
-                    {/* add controls */}
-                    <div className={cartStyles.cartItemQuantityContainer}>
-                      <button onClick={() => handleQuantityChange(item.id, -1)}
-                        className={cartStyles.cartItemQuantityButton}>
-                          <FiMinus />
-                        </button>
-                        <span className={cartStyles.cartItemQuantity}></span>
-                        <button onClick={() => handleQuantityChange(item.id, 1)}
-                          className={cartStyles.cartItemQuantity}>
-                            <FiPlus />
-                          </button>
-                    </div>
+              <div className="space-y-4 text-sm sm:text-base">
 
-                    <button onClick={() => removeFromCart(item.id)}
-                      className={cartStyles.cartItemRemoveButton}>
-                        <FiTrash2 className="mr-1" />
-                        Remove
-                      </button>
-                  </div>
-                ))}
-              </div>
-            </div>
-
-            {/* order summery */}
-            <div className="lg:col-span-1">
-              <div className={cartStyles.orderSummaryCard}>
-                <h2 className={cartStyles.orderSummaryTitle}>Order Summery</h2>
-
-                <div className="space-y-4 text-sm sm:text-base">
-                  
-                  <div className={cartStyles.orderSummaryRow}>
-                    <span className={cartStyles.orderSummaryLabel}>Subtotal</span>
-                    <span className={cartStyles.orderSummaryValue}>{getCartTotal().toFixed(2)}</span>
-                  </div>
-
-                  <div className={cartStyles.orderSummaryRow}>
-                    <span className={cartStyles.orderSummaryLabel}>Shopping</span>
-                    <span className={cartStyles.orderSummaryValue}>Free</span>
-                  </div>
-
-                  <div className={cartStyles.orderSummaryRow}>
-                    <span className={cartStyles.orderSummaryLabel}>Texes (5%)</span>
-                    <span className={cartStyles.orderSummaryValue}>{(getCartTotal() * 0.05).toFixed(2)}</span>
-                  </div>
-
-                  <div className={cartStyles.orderSummaryDivider}></div>
-
-                  <div className={cartStyles.orderSummaryTotalRow}>
-                    <span className={cartStyles.orderSummaryTotalLabel}>Total</span>
-                    <span className={cartStyles.orderSummaryTotalValue}>{(getCartTotal() * 1.05).toFixed(2)}</span>
-                  </div>
+                <div className={cartStyles.orderSummaryRow}>
+                  <span className={cartStyles.orderSummaryLabel}>Subtotal</span>
+                  <span className={cartStyles.orderSummaryValue}>{(getCartTotal || 0).toFixed(2)}</span>
                 </div>
 
-                <button className={cartStyles.checkoutButton}>
-                  Proceed to Checkout
-                </button>
+                <div className={cartStyles.orderSummaryRow}>
+                  <span className={cartStyles.orderSummaryLabel}>Shopping</span>
+                  <span className={cartStyles.orderSummaryValue}>Free</span>
+                </div>
 
-                <div className={cartStyles.continueShoppingBottom}>
-                  <Link to='/items' className={cartStyles.continueShopping}>
+                <div className={cartStyles.orderSummaryRow}>
+                  <span className={cartStyles.orderSummaryLabel}>Texes (5%)</span>
+                  <span className={cartStyles.orderSummaryValue}>{((getCartTotal || 0) * 0.05).toFixed(2)}</span>
+                </div>
+
+                <div className={cartStyles.orderSummaryDivider}></div>
+
+                <div className={cartStyles.orderSummaryTotalRow}>
+                  <span className={cartStyles.orderSummaryTotalLabel}>Total</span>
+                  <span className={cartStyles.orderSummaryTotalValue}>{((getCartTotal || 0) * 1.05).toFixed(2)}</span>
+                </div>
+              </div>
+
+              <button className={cartStyles.checkoutButton}>
+                Proceed to Checkout
+              </button>
+
+              <div className={cartStyles.continueShoppingBottom}>
+                <Link to='/items' className={cartStyles.continueShopping}>
                   <FiArrowLeft className="mr-2" />
                   Continue Shopping
-                  </Link>
-                </div>
+                </Link>
               </div>
             </div>
           </div>
         </div>
       </div>
-)
+    </div>
+  )
 };
 
 export default CartPage;
