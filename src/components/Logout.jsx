cfc7d208b0efa6e9aa094b5f9f5f710eef9df4f8
Hossam Albasuony <albasuonyh@gmail.com>
2026-04-02 13:42:47 +0200

    update project

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { FaSignOutAlt } from 'react-icons/fa'
+import { useAuth } from '../context/AuthContext'
+import toast from 'react-hot-toast'
 
 const Logout = () => {
+  const { logout } = useAuth();
 
   const handleLogout = () => {
-    localStorage.removeItem('authToken')
-    localStorage.removeItem('userData')
-
-    window.dispatchEvent(new Event('authStateChanged'))
+    logout();
+    window.dispatchEvent(new Event('authStateChanged'));
+    toast.success('Logged out successfully');
   }
   return (
     <div className='flex flex-col items-center justify-center h-full'>
-      <button onClick={handleLogout} 
-      className='flex items-center px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition'>
-        <FaSignOutAlt className= 'mr-2' />
+      <button onClick={handleLogout}
+        className='flex items-center px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition'>
+        <FaSignOutAlt className='mr-2' />
         Logout
       </button>
       <p className='mt-4 text-gray-600'>
--- src/pages/VerifyPaymentPage.jsx
+++ src/pages/VerifyPaymentPage.jsx
@@ -7,35 +7,22 @@ const VerifyPaymentPage = () => {
   const [statusMsg, setStatusMsg] = useState("Verifing Payment...")
   const { clearCart } = useCart()
   const navigate = useNavigate()
-  const {search} = useLocation()
+  const { search } = useLocation()
   useEffect(() => {
     const params = new URLSearchParams(search)
     const session_id = params.get("session_id")
     const payment_status = params.get("payment_status")
-    const token = localStorage.getItem("authToken")
-
-    if(payment_status === 'cancel'){
-      navigate('/checkout', {replace: true})
-      return
-    }
-
-    if (!session_id) {
-      setStatusMsg("No session Id Provided.")
-      return
-    }
-
     api.get('/api/orders/confirm', {
-      params: { session_id },
-      headers: token ? { Authorization: `Bearer ${token}` } : {},
+      params: { session_id }
     })
-    .then(() => {
-      clearCart()
-      navigate("/myorders", {replace: true})
-    })
-    .catch((err) => {
-      console.error("Confirmation error", err);
-      setStatusMsg('There was an error confirming your payment')
-    })
+      .then(() => {
+        clearCart()
+        navigate("/myorders", { replace: true })
+      })
+      .catch((err) => {
+        console.error("Confirmation error", err);
+        setStatusMsg('There was an error confirming your payment')
+      })
   }, [search, clearCart, navigate])
   return (
     <div className='min-h-screen flex items-center justify-center text-white'>
