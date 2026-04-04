5b57d53294a60eed0511f251dbd6d3cb581320f4
Hossam Albasuony <albasuonyh@gmail.com>
2026-04-04 16:19:07 +0200

    update project

--- src/constants/categories.jsx
+++ src/constants/categories.jsx
@@ -9,22 +9,22 @@ import {
   FiGrid
 } from 'react-icons/fi';
 import { 
-  GiAppleMaggot, 
+  GiFruitBowl, 
   GiBroccoli, 
   GiMilkCarton, 
   GiCupcake, 
-  GiFishBones, 
+  GiShrimp, 
   GiSteak, 
   GiBread 
 } from 'react-icons/gi';
 
 export const CATEGORIES = [
-  { id: 'fruits', name: 'Fruits', icon: <GiAppleMaggot />, color: 'rose' },
+  { id: 'fruits', name: 'Fruits', icon: <GiFruitBowl />, color: 'rose' },
   { id: 'vegetables', name: 'Vegetables', icon: <GiBroccoli />, color: 'emerald' },
   { id: 'dairy', name: 'Dairy', icon: <GiMilkCarton />, color: 'blue' },
   { id: 'beverages', name: 'Beverages', icon: <FiCoffee />, color: 'amber' },
   { id: 'snacks', name: 'Snacks', icon: <GiCupcake />, color: 'purple' },
-  { id: 'seafood', name: 'Seafood', icon: <GiFishBones />, color: 'cyan' },
+  { id: 'seafood', name: 'Seafood', icon: <GiShrimp />, color: 'cyan' },
   { id: 'bakery', name: 'Bakery', icon: <GiBread />, color: 'orange' },
   { id: 'meat', name: 'Meat', icon: <GiSteak />, color: 'red' },
 ];
